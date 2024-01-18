381285fc453cfa64c54379b04a739b3332d6bb56
Laurent Constantin <laurent.constantin@liip.ch>
2024-01-19 00:00:43 +0100

    Kobo Proxy: Forward to official store

--- public/index.php
+++ public/index.php
@@ -4,6 +4,13 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+if($_SERVER['HTTP_HOST'] === "books-proxy.while.ch"){
+    echo "<pre>";
+    var_dump(getallheaders());
+    echo file_get_contents('php://input');
+    exit;
+}
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
