92417d0b6728079234762a88695604043076bd71
Laurent Constantin <laurent.constantin@liip.ch>
2024-03-23 01:18:49 +0100

    remove hack on index.php

--- public/index.php
+++ public/index.php
@@ -4,13 +4,6 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-if($_SERVER['HTTP_HOST'] === "books-proxy.while.ch"){
-    echo "<pre>";
-    var_dump(getallheaders());
-    echo file_get_contents('php://input');
-    exit;
-}
-
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
